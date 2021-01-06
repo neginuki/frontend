532db9033d4a21a0d312ec43b8a1c0a8fd66ec0d
neginuki <mito.kunihiro@gmail.com>
2021-01-06 19:01:29 +0900

    SPA思い出した

--- nsake/src/App.svelte
+++ nsake/src/App.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+    import Router from "svelte-spa-router";
+    import Blank from "./contents/Blank.svelte";
+    import Body1 from "./contents/Body1.svelte";
+
+    import { link } from "svelte-spa-router";
+
+    const routes = {
+        "/": Blank,
+        "/body1": Body1,
+    };
 </script>
 
 <style>
 </style>
 
-<main />
+<main>
+    <div class="menu"><a href="#/">#</a></div>
+    <div class="menu"><a href="#/body1">Body1</a></div>
+
+    <Router {routes} />
+</main>
