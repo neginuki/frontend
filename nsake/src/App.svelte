4f41ac9d762e1a4e8f3aed07069926f1a72d13d3
neginuki <mito.kunihiro@gmail.com>
2021-01-06 18:24:33 +0900

    いらないので消す

--- nsake/src/App.svelte
+++ nsake/src/App.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-    import Header from "./Header.svelte";
 </script>
 
 <style>
 </style>
 
-<main>
-    <Header userName="なまえ" />
-</main>
+<main />
--- nsake/src/Header.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let userName: string = "ななしのごんべ";
-</script>
-
-<header>
-  <h1>Hello {userName}</h1>
-</header>
