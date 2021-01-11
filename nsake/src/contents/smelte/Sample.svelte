c8cc2ef0c91a835237d2bf241b8df9980df71d47
neginuki <mito.kunihiro@gmail.com>
2021-01-11 22:06:03 +0900

    smelte

--- nsake/src/contents/smelte/Sample.svelte
+++ nsake/src/contents/smelte/Sample.svelte
@@ -1,18 +1,125 @@
 <script lang="ts">
-    //
-    import "smelte/src/tailwind.css"; // main.ts でやるっぽい？
-    import { Select } from "smelte";
+    let breweryName = null;
+    let breweryNameKana = null;
+    let address = null;
+    let foundedYear = null;
+    let establishedDate = null;
+    let defunctDate = null;
+    let liquors = [];
+    let status = null;
+    let url = null;
+    let note = null;
 
-    const items = [
-        { value: 1, text: "One" },
-        { value: 2, text: "Two" },
-        { value: 3, text: "Three" },
-        { value: 4, text: "Four" },
-    ];
+    let data = [];
 
-    let label: string = "表示";
-    let value1 = "val";
+    const createBrewery = async (event) => {
+        event.currentTarget.innerHTML = "登録処理中です・・・";
+        event.currentTarget.disabled = true;
+
+        const response = await fetch("http://localhost:8080/breweries/", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                brewery_name: breweryName,
+                brewery_name_kana: breweryNameKana,
+                address: address,
+                founded_year: foundedYear,
+                established_date: establishedDate,
+                defunct_date: defunctDate,
+                liquors: liquors,
+                status: status,
+                url: url,
+                note: note,
+            }),
+        });
+        data = await response.json();
+    };
 </script>
 
-{value1}
-<Select {label} {items} on:change={(v) => (value1 = v.detail)} />
+<style>
+    div div {
+        margin: 20px;
+    }
+    textarea {
+        width: 500px;
+        height: 250px;
+    }
+</style>
+
+<main>
+    <h1>蔵元作成</h1>
+    <div>
+        <div>
+            <span>蔵名</span>
+            <input bind:value={breweryName} placeholder="e.g. 廣木酒造本店" />
+        </div>
+        <div>
+            <span>蔵名（かな）</span>
+            <input
+                bind:value={breweryNameKana}
+                placeholder="e.g. ひろきしゅぞうほんてん" />
+        </div>
+        <div>
+            <span>住所</span>
+            <input
+                bind:value={address}
+                placeholder="e.g. 福島県会津若松市XXX " />
+        </div>
+        <div>
+            <span>創業</span>
+            <input
+                type="number"
+                bind:value={foundedYear}
+                placeholder="e.g. 1978" />年
+        </div>
+        <div>
+            <span>設立</span>
+            <input bind:value={establishedDate} placeholder="e.g. 1919-12-31" />
+        </div>
+        <div>
+            <span>廃業</span>
+            <input bind:value={defunctDate} placeholder="e.g. 2000-01-01" />
+        </div>
+        <div>
+            <span>取り扱い</span>
+            <input
+                type="checkbox"
+                bind:group={liquors}
+                value="nihonshu" />日本酒
+            <input type="checkbox" bind:group={liquors} value="shochu" />焼酎
+            <input type="checkbox" bind:group={liquors} value="wine" />ワイン
+        </div>
+        <div>
+            <span>ステータス</span>
+            <input
+                type="radio"
+                name="status"
+                bind:group={status}
+                value="DRAFT" />未公開
+            <input
+                type="radio"
+                name="status"
+                bind:group={status}
+                value="RUNNING" />公開中
+            <input
+                type="radio"
+                name="status"
+                bind:group={status}
+                value="STOP" />停止中
+        </div>
+        <div>
+            <span>URL</span>
+            <input bind:value={url} placeholder="e.g. http://xxxx.yyyy" />
+        </div>
+        <div>
+            <span>備考</span>
+            <textarea
+                bind:value={note}
+                placeholder="e.g. オフレコですが、実は、、、" />
+        </div>
+    </div>
+    <button on:click|once={createBrewery}>登録</button>
+</main>
--- nsake/src/contents/sveltrap/Sample.svelte
+++ nsake/src/contents/sveltrap/Sample.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { Container, Button, Col, Row } from "sveltestrap/src";
-    import { Form, FormGroup, FormText, Input, Label } from "sveltestrap";
+    import {
+        Form,
+        FormGroup,
+        FormText,
+        Input,
+        CustomInput,
+        Label,
+    } from "sveltestrap";
 
-    let selectedStoreName = "";
     let stores: any = [];
 
     onMount(async () => {
@@ -14,21 +20,99 @@
 
         console.log(stores);
     });
+
+    let companyId = null;
+    let storeId = null;
+    let startDate = null;
+    let endDate = null;
+    let statusCode = null;
 </script>
 
 <style>
 </style>
 
 <Container class="mt-4">
-    <FormGroup>
-        <Input type="select" bind:value={selectedStoreName}>
-            <option value="">-- 店舗 --</option>
-            {#each stores as store}
-                <option value={store.id}>({store.code}) {store.name}</option>
-            {/each}
-        </Input>
-    </FormGroup>
-    {#if selectedStoreName}
-        <p>店舗名: {selectedStoreName}</p>
-    {/if}
+    <Row>
+        <Col>
+            <FormGroup>
+                <Label>法人名</Label>
+                <Input type="text" readonly value="(1234) ガッツリラーメン" />
+            </FormGroup>
+        </Col>
+    </Row>
+    <Row>
+        <Col>
+            <FormGroup>
+                <Label>店舗</Label>
+                <Input type="select" bind:value={storeId}>
+                    <option value="">-- 指定してください --</option>
+                    {#each stores as store}
+                        <option value={store.id}>
+                            ({store.code})
+                            {store.name}
+                        </option>
+                    {/each}
+                </Input>
+            </FormGroup>
+        </Col>
+    </Row>
+    <Row>
+        <Col>
+            <FormGroup>
+                <Label>期間(開始日)</Label>
+                <Input
+                    bind:value={startDate}
+                    type="date"
+                    placeholder="開始日" />
+            </FormGroup>
+        </Col>
+        <Col class="col-auto">～</Col>
+        <Col>
+            <FormGroup>
+                <Label>期間(終了日)</Label>
+                <Input bind:value={endDate} type="date" placeholder="終了日" />
+            </FormGroup>
+        </Col>
+    </Row>
+    <Row>
+        <Col>
+            <FormGroup>
+                <Label>状態</Label>
+                <CustomInput
+                    on:change={(e) => (statusCode = e.currentTarget.value)}
+                    type="radio"
+                    id="status1"
+                    value="status1"
+                    name="status"
+                    label="健康" />
+                <CustomInput
+                    bind:group={statusCode}
+                    type="radio"
+                    id="status2"
+                    value="status1"
+                    name="status"
+                    label="超健康" />
+                <CustomInput
+                    bind:group={statusCode}
+                    type="radio"
+                    id="status3"
+                    value="status1"
+                    name="status"
+                    label="無敵" />
+            </FormGroup>
+        </Col>
+    </Row>
+    <Row>
+        <Col>
+            <Button color="warning">登録</Button>
+        </Col>
+    </Row>
+</Container>
+
+<Container>
+    <div>companyId: {companyId}</div>
+    <div>storeId: {storeId}</div>
+    <div>startDate: {startDate}</div>
+    <div>endDate: {endDate}</div>
+    <div>statusCode: {statusCode}</div>
 </Container>
